1d05befc2e27075ca00e83d56becc6de0df50a5e
Chris Pilkington <chris.pilkington@gmail.com>
2015-05-25 23:19:51 +1000

    Added multiple colour pickers.

--- javascript/colourpicker.js
+++ javascript/colourpicker.js
@@ -3,18 +3,39 @@
 // https://gist.github.com/robtarr/1199770
 
 var app = {};
- 
+
 app.init = function(handler) {
-  app.$colors  = $('canvas.color-palette');
-  app.colorctx = app.$colors[0].getContext('2d');
- 
-  app.buildColorPalette(handler);
+  // Set up our handler
+  app.handler = handler;
+  
+  // Create four colour elements
+  app.colors = [ { element:null }, { element:null }, { element:null }, { element:null } ];
+  
+  // Create our contexts array
+  app.colorctx = [];
+  
+  // Create our positions arrays
+  app.colorEventX = [];
+  app.colorEventY = [];
+
+  app.initCanvas(0);
+  app.initCanvas(1);
+  app.initCanvas(2);
+  app.initCanvas(3);
+};
+
+app.initCanvas = function(uCanvasIndex) {
+  var elements = $('canvas.color-palette' + uCanvasIndex);
+  app.colors[uCanvasIndex].$element = elements;
+  app.colorctx[uCanvasIndex] = elements[0].getContext('2d');
+
+  app.buildColorPalette(uCanvasIndex);
 }
- 
+
 // Build Color palette
-app.buildColorPalette = function(handler) {
-  var gradient = app.colorctx.createLinearGradient(0, 0, app.$colors.width(), 0);
- 
+app.buildColorPalette = function(uCanvasIndex) {
+  var gradient = app.colorctx[uCanvasIndex].createLinearGradient(0, 0, app.colors[uCanvasIndex].$element.width(), 0);
+
   // Create color gradient
   gradient.addColorStop(0,    "rgb(255,   0,   0)");
   gradient.addColorStop(0.15, "rgb(255,   0, 255)");
@@ -23,36 +44,40 @@ app.buildColorPalette = function(handler) {
   gradient.addColorStop(0.67, "rgb(0,   255,   0)");
   gradient.addColorStop(0.84, "rgb(255, 255,   0)");
   gradient.addColorStop(1,    "rgb(255,   0,   0)");
- 
+
   // Apply gradient to canvas
-  app.colorctx.fillStyle = gradient;
-  app.colorctx.fillRect(0, 0, app.colorctx.canvas.width, app.colorctx.canvas.height);
-  
+  app.colorctx[uCanvasIndex].fillStyle = gradient;
+  app.colorctx[uCanvasIndex].fillRect(0, 0, app.colorctx[uCanvasIndex].canvas.width, app.colorctx[uCanvasIndex].canvas.height);
+
   // Create semi transparent gradient (white -> trans. -> black)
-  gradient = app.colorctx.createLinearGradient(0, 0, 0, app.$colors.height());
+  gradient = app.colorctx[uCanvasIndex].createLinearGradient(0, 0, 0, app.colors[uCanvasIndex].$element.height());
   gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
   gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
   gradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
   gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
- 
+
   // Apply gradient to canvas
-  app.colorctx.fillStyle = gradient;
-  app.colorctx.fillRect(0, 0, app.colorctx.canvas.width, app.colorctx.canvas.height);
- 
-  app.$colors.mousedown(function(e) {
- 
+  app.colorctx[uCanvasIndex].fillStyle = gradient;
+  app.colorctx[uCanvasIndex].fillRect(0, 0, app.colorctx[uCanvasIndex].canvas.width, app.colorctx[uCanvasIndex].canvas.height);
+
+  app.colors[uCanvasIndex].$element.mousedown(function(e) {
+    app.colorEventX[uCanvasIndex] = e.pageX - app.colors[uCanvasIndex].$element.offset().left;
+    app.colorEventY[uCanvasIndex] = e.pageY - app.colors[uCanvasIndex].$element.offset().top;
+
+    app.handler.OnColourSelected(uCanvasIndex);
+
     // Track mouse movement on the canvas if the mouse button is down
     $(document).mousemove(function(e) {
-      app.colorEventX = e.pageX - app.$colors.offset().left;
-      app.colorEventY = e.pageY - app.$colors.offset().top;
-      
-      handler.OnColourSelected();
+      app.colorEventX[uCanvasIndex] = e.pageX - app.colors[uCanvasIndex].$element.offset().left;
+      app.colorEventY[uCanvasIndex] = e.pageY - app.colors[uCanvasIndex].$element.offset().top;
+
+      app.handler.OnColourSelected(uCanvasIndex);
     });
-  
+
     // Get the color at the current mouse coordinates
-    app.colorTimer = setInterval(app.getColor, 50);
+    app.colorTimer = setInterval(function() { app.getColor(uCanvasIndex); }, 50);
   })
- 
+
   // On mouseup, clear the interval and unbind the mousemove event,
   // it should only happen if the button is down
   .mouseup(function(e) {
@@ -61,11 +86,12 @@ app.buildColorPalette = function(handler) {
   });
 };
 
-app.getColor = function(e) {
-  var newColor;
- 
-  imageData = app.colorctx.getImageData(app.colorEventX, app.colorEventY, 1, 1);
-  app.selectedColor = 'rgb(' + imageData.data[4] + ', ' + imageData.data[5] + ', ' + imageData.data[6] + ')';
-  
+app.getColor = function(uCanvasIndex) {
+  imageData = app.colorctx[uCanvasIndex].getImageData(app.colorEventX[uCanvasIndex], app.colorEventY[uCanvasIndex], 1, 1);
+
   console.log("app.getColor RGB(" + imageData.data[4] + ", " + imageData.data[5] + ", " + imageData.data[6] + ")");
+
+  var colour = { r : imageData.data[4], g : imageData.data[5], b : imageData.data[6] };
+  
+  return colour;
 };
