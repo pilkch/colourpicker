3a951a7dda5e64dc9447dac6d94b850fa105f8a6
Chris Pilkington <chris.pilkington@gmail.com>
2016-09-10 22:45:28 +1000

    Added hex and float with sliders, inputs and text output (Layout is ugly).

--- javascript/colourpicker.js
+++ javascript/colourpicker.js
@@ -4,12 +4,15 @@
 
 var app = {};
 
-app.init = function(handler) {
+app.init = function (handler) {
+  // Set up a constant for the number of colour pickers
+  app.nColourPickers = 5;
+
   // Set up our handler
   app.handler = handler;
   
   // Create four colour elements
-  app.colours = [ { element:null }, { element:null }, { element:null }, { element:null } ];
+  app.colours = [ { element: null }, { element: null }, { element: null }, { element: null }, { element: null } ];
   
   // Create our contexts array
   app.colourctx = [];
@@ -21,19 +24,14 @@ app.init = function(handler) {
   app.crossHairPositionX = 190;
   app.crossHairPositionY = 77;
 
-  app.initCanvas(0);
-  app.initCanvas(1);
-  app.initCanvas(2);
-  app.initCanvas(3);
+  for (var i = 0; i < app.nColourPickers; i++) app.initCanvas(i);
   
   // Create the first gradient
   app.buildColourPalette(0);
   app.storeGradientImage(0);
   
   // Copy the gradient from the first colour palette to the other colour palettes
-  app.copyColourPaletteImageFromTheFirstTo(1);
-  app.copyColourPaletteImageFromTheFirstTo(2);
-  app.copyColourPaletteImageFromTheFirstTo(3);
+  for (var i = 1; i < app.nColourPickers; i++) app.copyColourPaletteImageFromTheFirstTo(i);
   
   // Apply our colour blind filters
   app.applyColourBlindFilterProtanopia(1);
@@ -41,9 +39,7 @@ app.init = function(handler) {
   app.applyColourBlindFilterTritanopia(3);
   
   // Get copies of our gradient images
-  app.storeGradientImage(1);
-  app.storeGradientImage(2);
-  app.storeGradientImage(3);
+  for (var i = 1; i < app.nColourPickers; i++) app.storeGradientImage(i);
   
   // Update our cross hairs
   app.updateAllColourPickersFrom(0);
@@ -58,23 +54,23 @@ app.initCanvas = function(uCanvasIndex) {
   app.colours[uCanvasIndex].$element.mousedown(function(e) {
     app.crossHairPositionX = e.pageX - app.colours[uCanvasIndex].$element.offset().left;
     app.crossHairPositionY = e.pageY - app.colours[uCanvasIndex].$element.offset().top;
-
-    app.handler.OnColourSelected(uCanvasIndex);
     
     app.updateAllColourPickersFrom(uCanvasIndex);
+
+    app.handler.OnColourSelected(uCanvasIndex);
 
     // Track mouse movement on the canvas if the mouse button is down
     $(document).mousemove(function(e) {
       app.crossHairPositionX = e.pageX - app.colours[uCanvasIndex].$element.offset().left;
       app.crossHairPositionY = e.pageY - app.colours[uCanvasIndex].$element.offset().top;
-
-      app.handler.OnColourSelected(uCanvasIndex);
       
       app.updateAllColourPickersFrom(uCanvasIndex);
+
+      app.handler.OnColourSelected(uCanvasIndex);
     });
 
     // Get the colour at the current mouse coordinates
-    app.colourTimer = setInterval(function() { app.getColour(uCanvasIndex); }, 50);
+    app.colourTimer = setInterval(function() { app.getColourUnderCrossHair(uCanvasIndex); }, 50);
   })
 
   // Handle mouse up
@@ -128,9 +124,7 @@ app.copyColourPaletteImageFromTheFirstTo = function(uCanvasIndex) {
 }
 
 app.updateAllColourPickersFrom = function(uCanvasIndex) {
-  for (var i = 0; i < 4; i++) {
-    app.render(i);
-  }
+  for (var i = 0; i < app.nColourPickers; i++) app.render(i);
 }
 
 app.render = function(uCanvasIndex) {
@@ -161,34 +155,27 @@ app.render = function(uCanvasIndex) {
   context.stroke();
 
   // Draw the RGB value
-  var colour = app.getColour(uCanvasIndex);
+  var colour = app.getColourUnderCrossHair(uCanvasIndex);
   var sText = "RGB(" + colour.r + ", " + colour.g + ", " + colour.b + ")";
   context.font = "20px Georgia"
   context.fillStyle = "#000000";
   context.fillText(sText, 6, 22);
 }
 
-app.getOriginalPixel = function(x, y) {
-  // Get a 1x1 image of our pixel
-  imageData = app.colourctx[0].getImageData(x, y, 1, 1);
-
-  var colour = { r : imageData.data[0], g : imageData.data[1], b : imageData.data[2] };
-
-  return colour;
+app.getOriginalPixel = function (x, y) {
+  // Get a pixel from the first canvas
+  return app.getPixel(0, x, y);
 };
 
-app.getColour = function(uCanvasIndex) {
+app.getColourUnderCrossHair = function (uCanvasIndex) {
   return app.getPixel(uCanvasIndex, app.crossHairPositionX, app.crossHairPositionY);
 };
 
 app.getPixel = function(uCanvasIndex, x, y) {
   // Get a 1x1 image of our pixel
-  var imageData = app.gradientImage[uCanvasIndex];
+  imageData = app.colourctx[uCanvasIndex].getImageData(x, y, 1, 1);
 
-  var width = app.colours[uCanvasIndex].$element.width();
-
-  var index = 4 * ((y * width) + x);
-  var colour = { r : imageData.data[index], g : imageData.data[index + 1], b : imageData.data[index + 2] };
+  var colour = { r: imageData.data[0], g: imageData.data[1], b: imageData.data[2] };
 
   return colour;
 };
@@ -205,6 +192,39 @@ app.setPixel = function(uCanvasIndex, x, y, colour) {
   // Set the new image data
   app.colourctx[uCanvasIndex].putImageData(imageData, x, y);  
 };
+
+// Stolen from:
+// https://stackoverflow.com/questions/16835070/create-gradient-for-color-selection-with-html5-canvas-all-possible-rgb-colors
+// Takes a colour in RGB, finds the position of a HSV pixel that matches, updates the crosshair position and updates all colour pickers
+app.setCanvas0Colour = function (colourRGB) {
+  var colourHSV = RGBtoHSV(colourRGB);
+
+  var width = app.colours[0].$element.width();
+  var height = app.colours[0].$element.height();
+
+  // NOTE: Width is flipped from left to right because our colour palettes are flipped left to right
+  var x = width - (width / 360 * colourHSV.h);
+  var y = 0;
+  var m = height / 2;
+
+  if (colourHSV.s === 100 && colourHSV.v === 100) {
+    y = m;
+  } else if (colourHSV.v === 100 && colourHSV.s < 100) {
+    y = m / 100 * colourHSV.s;
+  } else if (colourHSV.s === 100 && colourHSV.v < 100) {
+    y = m / 100 * (100 - colourHSV.v) + m;
+  }
+
+  x = (x + 0.5) | 0; // Convert to integer
+  y = (y + 0.5) | 0;
+
+  // Set our cursor position
+  app.crossHairPositionX = x;
+  app.crossHairPositionY = y;
+
+  // Update our cross hairs
+  app.updateAllColourPickersFrom(0);
+}
 
 // Protanopia (Reds are greatly reduced (1% of men))
 app.applyColourBlindFilterProtanopia = function(uCanvasIndex) {
--- javascript/util.js
+++ javascript/util.js
@@ -10,3 +10,11 @@ function ASSERT(condition, message) {
     throw message; // Fallback
   }
 }
+
+// Stolen from here:
+// http://www.webdesignerdepot.com/2013/03/how-to-create-a-color-picker-with-html5-canvas/
+function IntToHex(value) {
+  value = parseInt(value, 10);
+  if (isNaN(value)) return "00";
+  value = Math.max(0, Math.min(value, 255)); return "0123456789ABCDEF".charAt((value - (value % 16)) / 16) + "0123456789ABCDEF".charAt(value % 16);
+}
